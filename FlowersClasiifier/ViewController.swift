9dc4818591dd42bb88e09e2c3b5453b7b95ff076
Nazari <ryannazari21@gmail.com>
2019-01-28 12:29:03 +0200

    add detect ciImage

--- FlowersClasiifier/ViewController.swift
+++ FlowersClasiifier/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+    var pickedImage = CIImage?.self
 
+
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("test push control")
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
     }
 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImge = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            guard let convertedCIImage = CIImage(image: userPickedImge) else {
+                fatalError("Could not convert image to ciImage")
+            }
+            
+            imageView.image = userPickedImge
+            
+            detect(image: convertedCIImage)
+            
+        }
+        
+        
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
+    
+
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            
+            fatalError("Cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    
 }
 
